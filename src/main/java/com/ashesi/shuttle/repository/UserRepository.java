dd3f413ec7ed10d359f87699e8390f3ec6645e14
SMarco2310 <sossoumarcetienne@icloud.com>
2025-02-03 21:14:49 +0000

    updated the UserRepository.java

--- src/main/java/com/ashesi/shuttle/repository/UserRepository.java
+++ src/main/java/com/ashesi/shuttle/repository/UserRepository.java
@@ -2,6 +2,7 @@ package com.ashesi.shuttle.repository;
 
 import com.ashesi.shuttle.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface UserRepository extends JpaRepository<User, Integer> {
 
@@ -10,5 +11,6 @@ public interface UserRepository extends JpaRepository<User, Integer> {
 
     User findByID(int id);
 
-
+//    @Query("SELECT u FROM User u WHERE u.email = ?1")
+    User findByEmail(String email);
 }
