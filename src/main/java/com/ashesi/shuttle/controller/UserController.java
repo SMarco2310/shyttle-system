52fe69063cbfeb55810f912bfb8ad250ed289cd0
SMarco2310 <sossoumarcetienne@icloud.com>
2025-02-05 23:28:27 +0000

    Implement UserController and UserService with CRUD operations for User management

--- src/main/java/com/ashesi/shuttle/controller/UserController.java
+++ src/main/java/com/ashesi/shuttle/controller/UserController.java
@@ -1,16 +1,62 @@
 package com.ashesi.shuttle.controller;
 
+import com.ashesi.shuttle.model.User;
 import com.ashesi.shuttle.service.UserService;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
 
 @RestController
-@RequestMapping
-
-
+@RequestMapping("/users")
 public class UserController {
 
+    private final UserService userService;
 
-    private UserService userService;
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping
+    public List<User> getAllUsers() {
+        return userService.findAllUsers();
+    }
+
+    @GetMapping("/users")
+    public ResponseEntity<User> getUserById(@PathVariable Integer id) {
+        Optional<User> user = userService.findUserById(id);
+        return user.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
+    }
+
+    @PostMapping
+    public User createUser(@RequestBody User user) {
+        return userService.saveUser(user);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<User> updateUser(@PathVariable Integer id, @RequestBody User userDetails) {
+        Optional<User> user = userService.findUserById(id);
+        if (user.isPresent()) {
+            User updatedUser = user.get();
+            updatedUser.setFirstName(userDetails.getFirstName());
+            updatedUser.setLastName(userDetails.getLastName());
+            updatedUser.setEmail(userDetails.getEmail());
+            updatedUser.setPhoneNumber(userDetails.getPhoneNumber());
+            userService.saveUser(updatedUser);
+            return ResponseEntity.ok(updatedUser);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteUser(@PathVariable Integer id) {
+        if (userService.existsById(id)) {
+            userService.deleteUserById(id);
+            return ResponseEntity.noContent().build();
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
 }
--- src/main/java/com/ashesi/shuttle/service/UserService.java
+++ src/main/java/com/ashesi/shuttle/service/UserService.java
@@ -1,4 +1,44 @@
 package com.ashesi.shuttle.service;
 
+import com.ashesi.shuttle.model.User;
+import com.ashesi.shuttle.repository.UserRepository;
+//import com.ashesi.shuttle.exception.UserNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
 public class UserService {
+
+    private final UserRepository userRepository;
+
+    @Autowired
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public List<User> findAllUsers() {
+        return userRepository.findAll();
+    }
+
+    public Optional<User> findUserById(Integer id) {
+        return userRepository.findById(id);
+    }
+
+    public User saveUser(User user) {
+        return userRepository.save(user);
+    }
+
+    public void deleteUserById(Integer id) {
+//        if (!userRepository.existsById(id)) {
+//            throw new UserNotFoundException("User not found with id: " + id);
+//        }
+        userRepository.deleteById(id);
+    }
+
+    public boolean existsById(Integer id) {
+        return userRepository.existsById(id);
+    }
 }
